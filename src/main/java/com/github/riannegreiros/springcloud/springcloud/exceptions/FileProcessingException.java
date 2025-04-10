d00a1397b5f19d3080571e75e5e81b9f5c07f726
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-04-10 09:02:30 -0300

    chore: fresh start for the AISpringMind

--- src/main/java/com/github/riannegreiros/springcloud/springcloud/exceptions/FileProcessingException.java
+++
@@ -1,8 +0,0 @@
-package com.github.riannegreiros.springcloud.springcloud.exceptions;
-
-public class FileProcessingException extends RuntimeException {
-
-    public FileProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- src/main/java/com/github/riannegreiros/springcloud/springcloud/exceptions/ImageNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.github.riannegreiros.springcloud.springcloud.exceptions;
-
-public class ImageNotFoundException extends RuntimeException {
-
-    public ImageNotFoundException(String message) {
-        super(message);
-    }
-}
